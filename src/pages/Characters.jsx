9191a00365dcbd130ff952bd6c9ce9cf1b7f49f3
18-Andres-AD <darwinandresdiazacosta7@gmail.com>
2025-10-14 23:08:26 -0500

    Boorador

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,32 +1,3 @@
-// import { useState, useEffect } from 'react'
-// import CardCharacters from '../components/Characters/CharacterCard'
-
-
-// const Characters = () => {
-
-//   const [characters, setCharacters] = useState([])
-
-//   useEffect(() => {
-//     fetch('https://thesimpsonsapi.com/api/characters')
-//       .then(response => response.json())
-//       .then(data =>
-//         setCharacters(data.results)
-//       )
-//   }, [])
-
-//   return (
-//     <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: '20px'}}>
-//       {
-//         characters.length > 0 ? characters.map(
-//           character => <CardCharacters key={character.id} data={character} />
-//         ) : <p>Cargando...</p>
-//       }
-//       </div>
-//   )
-// }
-
-// export default Characters
-
 import { useState, useEffect } from 'react'
 import CardCharacters from '../components/Characters/CharacterCard'
 import Pagination from '../components/Pagination/Pagination'
