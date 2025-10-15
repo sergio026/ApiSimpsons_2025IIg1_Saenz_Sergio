763cb661b41daa8a5ca3a3d02e2b26187a0464f6
sergio026 <saenzmorales65@gmail.com>
2025-10-14 19:41:21 -0500

    Eliminar script de lint en package.json y agregar espacio en blanco en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   base: '/ApiSimpsons_2025IIg2_Saenz_Sergio_Diaz_Darwin/', 
