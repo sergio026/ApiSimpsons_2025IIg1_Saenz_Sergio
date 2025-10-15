dda386e9596b3d61bbfeca03d8717f8082790a33
18-Andres-AD <darwinandresdiazacosta7@gmail.com>
2025-10-14 23:03:36 -0500

    Centrar Memu

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,3 +1,32 @@
+// import { useState, useEffect } from 'react'
+// import CardCharacters from '../components/Characters/CharacterCard'
+
+
+// const Characters = () => {
+
+//   const [characters, setCharacters] = useState([])
+
+//   useEffect(() => {
+//     fetch('https://thesimpsonsapi.com/api/characters')
+//       .then(response => response.json())
+//       .then(data =>
+//         setCharacters(data.results)
+//       )
+//   }, [])
+
+//   return (
+//     <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: '20px'}}>
+//       {
+//         characters.length > 0 ? characters.map(
+//           character => <CardCharacters key={character.id} data={character} />
+//         ) : <p>Cargando...</p>
+//       }
+//       </div>
+//   )
+// }
+
+// export default Characters
+
 import { useState, useEffect } from 'react'
 import CardCharacters from '../components/Characters/CharacterCard'
 import Pagination from '../components/Pagination/Pagination'
@@ -18,19 +47,24 @@ const Characters = () => {
     }
   }
 
-  useEffect(() => {fetchCharacters(page)}, [page])
+  useEffect(() => { fetchCharacters(page) }, [page])
 
   const handlePrev = () => setPage((prev) => Math.max(prev - 1, 1))
   const handleNext = () => setPage((prev) => Math.min(prev + 1, totalPages))
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: '20px'}}>
-        {characters.length > 0 ? (
-          characters.map((character) => <CardCharacters key={character.id} data={character} />)
-        ) : (
-          <p>Cargando personajes...</p>
-        )}
-      <Pagination page={page} totalPages={totalPages} onPrev={handlePrev} onNext={handleNext} />
+    <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: '20px' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap',justifyContent: 'center',alignItems: 'flex-start',gap: '20px',width: '100%', }} >
+        
+      {characters.length > 0 ? (
+        characters.map((character) => <CardCharacters key={character.id} data={character} />)
+      ) : (
+        <p>Cargando personajes...</p>
+      )}
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <Pagination page={page} totalPages={totalPages} onPrev={handlePrev} onNext={handleNext} />
+      </div>
     </div>
   )
 }
